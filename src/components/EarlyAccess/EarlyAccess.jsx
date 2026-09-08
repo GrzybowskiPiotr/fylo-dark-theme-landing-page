a125653952e0b6e780fdaea049453ada96babe4e
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2026-09-08 21:07:34 +0200

    feat: add footer structure

--- src/components/EarlyAccess/EarlyAccess.jsx
+++ src/components/EarlyAccess/EarlyAccess.jsx
@@ -20,12 +20,13 @@ export function EarlyAccess(){
   return <section className={style.container}>
     <h2>Get early access today</h2>
     <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
-    <form onSubmit={handleSubmit} >
+    <form onSubmit={handleSubmit} className={style.form}>
       <label htmlFor="email">
-      <input type="text" id="email" name="email" autoComplete="on" placeholder="email@example.com" onChange={(e) => onInputChange(e)}/>
+      <input type="text" id="email" name="email" autoComplete="on" placeholder="email@example.com" onChange={(e) => onInputChange(e)}
+      className={style.pillShape}/>
       </label>
       <p className={`${style.errorMessage} ${error ? "" : style.hide}`}>Error, please check your email</p>
-      <button type="submit">get started for free</button>
+      <button type="submit" className={style.pillShape}>get started for free</button>
     </form>
     </section>
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,5 +3,13 @@ import style from "./Footer.module.css"
 export function Footer(){
   return <footer className={style.container}>
     <h2>Footer</h2>
+  <p>Icon fylo</p>
+  <address>
+    <ul>
+      <li><img/><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p></li>
+      <li><img/><a href="tel:+15431234567">+1-543-123-4567</a></li>
+      <li><img/><a href="mailto:example@fylo.com"></a>example@fylo.com</li>
+    </ul>
+  </address>
   </footer>
 }
